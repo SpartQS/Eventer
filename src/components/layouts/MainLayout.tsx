21e859824fe387d0ffe03139c7f74ca0cedeed32
AslanI22 <reshetov.aslan.i22@gmail.com>
2025-05-23 15:50:56 +0300

    add components on ProfilePage

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,14 +3,24 @@ import { AppSidebar } from "@/components/app-sidebar";
 import {
     SidebarInset,
     SidebarProvider,
+    SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 
 export const MainLayout = () => {
     return (
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <Outlet />
+                <header className="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 border-b bg-background">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mr-2 h-4" />
+                    </div>
+                </header>
+                <main className="flex-1">
+                    <Outlet />
+                </main>
             </SidebarInset>
         </SidebarProvider>
     );
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,8 +1,15 @@
+import { ProfileCard } from "@/components/ProfileCard"
+import { NotificationsEvents } from "@/components/NotificationsEvents"
 
 export function ProfilePage() {
     return (
-        <>
-            Привет
-        </>
+        <div className="flex gap-6 p-6">
+            <ProfileCard />
+            <div className="flex-1">
+                <div className="h-full">
+                    <NotificationsEvents />
+                </div>
+            </div>
+        </div>
     )
 }
