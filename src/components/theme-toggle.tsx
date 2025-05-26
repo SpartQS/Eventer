e2983f9ce099a524f1a93f69fee9bee7fd9dc3db
AslanI22 <reshetov.aslan.i22@gmail.com>
2025-05-26 12:41:10 +0300

    redact theme

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
@@ -30,9 +29,6 @@ export function ThemeToggle() {
                 </DropdownMenuItem>
                 <DropdownMenuItem onClick={() => setTheme("dark")}>
                     Темная
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    Системная
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
